2708aa0a75db121744b665e813263b6cef6c17fb
Rosselle Macabata <macabar@quest-OptiPlex-9020>
2016-02-10 20:16:53 -0800

    Uploading Room and SceneCard class

---
+++ Rooms.java
@@ -0,0 +1,15 @@
+public abstract class Room{
+   //protected ArrayList<> adjacentRooms;
+   protected String title;
+   
+   
+   public Room(String title){
+      //this.adjacentRooms = adjacentRooms;
+      this.title = title;
+   }
+
+   public void moveTo(){
+
+   }
+}
+
