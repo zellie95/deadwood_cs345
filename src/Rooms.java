7bfe2ac7f09c3466d5ffa7ef8ff68f390e81ecd3
Theodore Weber <webert3@quest-OptiPlex-9020>
2016-02-13 12:17:36 -0800

    Changed room structure.

---
+++ src/Rooms.java
@@ -0,0 +1,13 @@
+public abstract class Room {
+   //protected ArrayList<> adjacentRooms;
+    private String title;
+
+    public Room(String title){
+      //this.adjacentRooms = adjacentRooms;
+      this.title = title;
+   }
+
+   public void moveTo(){
+
+   }
+}
